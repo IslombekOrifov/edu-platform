ae81197645c2f41d0a3b9e9d7c8ec12e6e994e13
IslombekOrifov <islombekorifov0199@gmail.com>
2023-11-07 18:27:38 +0500

    added Module model and admin.py changed

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin): 
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "middle_name", "email")}),
+        (_("Permissions"),{"fields": ("is_active", "is_staff", "is_superuser", "is_deleted", "groups", "user_permissions",),},),
+        (_("Important dates"), {"fields": ("first_login", "last_login", "date_joined")}),
+        (_("Custom info"), {"fields": ("phone", "avatar")}),
+    )
